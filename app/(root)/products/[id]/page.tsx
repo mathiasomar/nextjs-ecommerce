360296a5aa02ccf9cb82216744966c6a23d784c5
mathiasomar <mathiasomar@gmail.com>
2025-11-11 11:50:58 +0300

    product details page

--- app/(root)/products/[id]/page.tsx
+++ app/(root)/products/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductType } from "@/app/types";
 import Container from "@/components/container";
+import ProductInteraction from "@/components/product-interaction";
 import Image from "next/image";
 
 // TEMPORARY DATA
@@ -43,7 +44,51 @@ const ProductPage = async ({
         />
       </div>
       {/* DETAILS */}
-      <div className="w-full lg:w-7/12"></div>
+      <div className="w-full lg:w-7/12 flex flex-col gap-4">
+        <h1 className="2xl font-medium">{product.name}</h1>
+        <p className="text-gray-500">{product.description}</p>
+        <h2 className="text-2xl font-semibold">
+          Ksh.{product.price.toFixed(2)}
+        </h2>
+        {/* ITERACTIONS */}
+        <ProductInteraction
+          product={product}
+          selectedSize={selectedSize}
+          selectedColor={selectedColor}
+        />
+        {/* CARD INFO */}
+        <div className="flex items-center gap-2 mt-4">
+          <Image
+            src="/klarna.png"
+            alt="klarna"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+          <Image
+            src="/cards.png"
+            alt="cards"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+          <Image
+            src="/stripe.png"
+            alt="stripe"
+            width={50}
+            height={25}
+            className="rounded-md"
+          />
+        </div>
+        <p className="text-gray-500 text-xs">
+          By clicking Pay Now, you agree to our{" "}
+          <span className="underline hover:text-black">Terms & Conditions</span>{" "}
+          and <span className="underline hover:text-black">Privacy Policy</span>
+          . You authorize us to charge your selected payment method for the
+          total amount shown. All sales are subject to our return and{" "}
+          <span className="underline hover:text-black">Refund Policies</span>.
+        </p>
+      </div>
     </Container>
   );
 };
